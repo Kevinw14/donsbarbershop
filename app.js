b6bce736884b9471896c2bf6c42c92ae3f98d97e
Kevin <kevinlw14@gmail.com>
2020-02-12 22:40:37 -0500

    Upload Test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000 | process.env.PORT
+const port = 3000 || process.env.PORT
 
 app.set('view engine', 'ejs')
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listen on port ${port}`))
 
 module.exports = app
