b23dbba51415380768715d8121f287cafb5a4d75
Kevin <kevinlw14@gmail.com>
2020-02-17 21:03:14 -0500

    Finished Map

--- javascript/home.js
+++ javascript/home.js
@@ -3,6 +3,6 @@ $('.carousel-item:first').addClass('active');
 function initMap() {
     var dons = { lat: 36.279123, lng: -80.359686 };
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 16, center: dons });
-    var marker = new google.maps.Marker({ position: dons, map: map });
+        document.getElementById('map'), { zoom: 16, center: dons, fullscreenControl: false });
+    new google.maps.Marker({ position: dons, map: map });
 }
